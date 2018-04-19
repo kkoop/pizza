efabb550961080b36fd197aa0988c30b7c8e6c1b
Karsten Koop <karsten.koop@gmx.de>
2018-04-19 23:23:38 +0200

    Seitentitel aus config.php

--- website/error_pages/404.php
+++ website/error_pages/404.php
@@ -4,17 +4,17 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>LD Essensbestellung - Error 404</title>
-  <link href="/css/style.css" rel="stylesheet">
+  <title><?= K_PRODUCT_NAME ?> - Error 404</title>
+  <link href="<?=$baseUrl?>/css/style.css" rel="stylesheet">
 </head>
 
 <body>
   <!-- Fixed navbar -->
-  <nav class="navbar navbar-default navbar-fixed-top">
+  <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
       <div class="navbar-header">
-        <a class="navbar-brand" href="/">
-          <img src="/img/Logo.svg" onerror="this.onerror=null; this.src='img/Logo.png'" alt="logo">
+        <a class="navbar-brand" href="<?=$baseUrl?>/">
+          <img src="<?=$baseUrl?>/img/Logo.svg" onerror="this.onerror=null; this.src='<?=$baseUrl?>/img/Logo.png'" alt="logo">
         </a>
       </div>
     </div>
@@ -22,7 +22,7 @@
 
   <div class="container" role="main">
     <div class="col-sm-6 col-sm-push-6">
-      <img src="/img/Logo_broken.svg">
+      <img src="<?=$baseUrl?>/img/Logo_broken.svg">
     </div>
     <div class="col-sm-6 col-sm-pull-6">
       <h1>Fehler 404 - Nicht gefunden</h1>
