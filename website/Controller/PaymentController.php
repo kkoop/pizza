621cc607d707ab70a4ff9d7cd2cdd77d35bd86f5
Karsten Koop <koop@hiskp.uni-bonn.de>
2018-04-21 10:28:21 +0200

    Anzeige von Zahlungen

--- website/Controller/PaymentController.php
+++ website/Controller/PaymentController.php
@@ -6,5 +6,18 @@ class PaymentController extends  Controller
 {
   public function indexAction()
   {
+    $this->view->setVars(['title' => "Zahlungen"]);
+    $startTime = strtotime("-1 month");
+    $endTime   = time();
+    $this->view->setVars(['startDate' => strftime("%x", $startTime),
+                          'endDate'   => strftime("%x", $endTime),
+                          'payments'  => Model\Payment::getForUser($startTime, $endTime)]);
+                          
+  }
+  
+  public function editAction()
+  {
+    $this->view->setVars(['title' => "Zahlungen"]);
+    
   }
 }
--- website/Model/Payment.php
+++ website/Model/Payment.php
@@ -5,6 +5,24 @@ class Payment
 {
   public $id;
   public $time;
-  public $user;
+  public $fromId;
+  public $fromName;
+  public $toId;
+  public $toName;
   public $amount;
+  
+  static function getForUser($startDate, $endDate)
+  {
+    $stmt = Db::prepare("SELECT payment.id,UNIX_TIMESTAMP(time) AS time,amount,".
+        "fromuser AS fromId,touser AS toId,fuser.name AS fromName,tuser.name AS toName ".
+      "FROM payment ".
+      "JOIN user AS fuser ON fuser.id=payment.fromuser ".
+      "JOIN user AS tuser ON tuser.id=payment.touser ".
+      "WHERE (payment.fromuser=:user OR payment.touser=:user2) ".
+        "AND time>=FROM_UNIXTIME(:start) AND time<=FROM_UNIXTIME(:end)");
+    $stmt->execute([":start" => $startDate, ":end" => $endDate,
+                    ":user"  => $_SESSION['user']->id,
+                    ":user2" => $_SESSION['user']->id]);
+    return $stmt->fetchAll(\PDO::FETCH_CLASS, get_class());
+  }
 }
